da6b7c6184d6eaeb2bf4e81e962340b41514abd6
Nadia Laban <labannadia5@gmail.com>
2024-06-05 01:46:31 +0300

    add event bus

--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router/index.js'
+import EventBus from "@/event-bus.js";
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.config.globalProperties.EventBus = EventBus
 
 app.mount('#app')
